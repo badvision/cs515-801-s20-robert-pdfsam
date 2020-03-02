08cce3125c45910eba4761cd5ec2c69dab7de0d1
Brendan Robert <brobert@adobe.com>
2020-03-01 23:08:13 -0800

    Added test case and repair for range parsing issue #1

--- pdfsam-core/src/main/java/org/pdfsam/support/params/ConversionUtils.java
+++ pdfsam-core/src/main/java/org/pdfsam/support/params/ConversionUtils.java
@@ -1,11 +1,11 @@
-/* 
+/*
  * This file is part of the PDF Split And Merge source code
  * Created on 22 giu 2016
  * Copyright 2017 by Sober Lemur S.a.s. di Vacondio Andrea (info@pdfsam.org).
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as 
- * published by the Free Software Foundation, either version 3 of the 
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -18,16 +18,19 @@
  */
 package org.pdfsam.support.params;
 
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
-import static org.sejda.conversion.AdapterUtils.splitAndTrim;
-
 import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.Set;
-
+import java.util.TreeSet;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 import org.pdfsam.i18n.DefaultI18nContext;
 import org.sejda.common.collection.NullSafeSet;
 import org.sejda.conversion.exception.ConversionException;
 import org.sejda.model.pdf.page.PageRange;
+
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static org.sejda.conversion.AdapterUtils.splitAndTrim;
 
 /**
  * @author Andrea Vacondio
@@ -40,7 +43,82 @@ public final class ConversionUtils {
     }
 
     /**
-     * @return the {@link PageRange} set for the given string, an empty set otherwise.
+     * Simplify a collection of page ranges to collapse adjacent ranges and deal
+     * with intersections as well.
+     *
+     * This is not a descructive operation to the original set
+     *
+     * @param ranges
+     * @return normalized ranges
+     */
+    public static Set<PageRange> normalizePageRangeSet(Set<PageRange> ranges) {
+        if (ranges.isEmpty()) {
+            return ranges;
+        }
+
+        AtomicInteger unboundStart = new AtomicInteger(-1);
+        Set<Integer> allPages = new TreeSet<>();
+
+        // First find any unbounded sets, pick the one with the lowest start number
+        for (PageRange range : ranges) {
+            if (range.isUnbounded()
+                    && (unboundStart.get() < 0 || unboundStart.get() > range.getStart())) {
+                unboundStart.set(range.getStart());
+            }
+        }
+
+        ranges.stream()
+                // Now for all bounded sets
+                .filter((range) -> (!range.isUnbounded()))
+                // track all page numbers
+                .filter(r -> unboundStart.get() < 0 || r.getStart() < unboundStart.get())
+                .forEach(
+                        range -> IntStream.rangeClosed(range.getStart(), range.getEnd())
+                                .forEach(allPages::add)
+                );
+
+        // Expand the unbounded range downward if possible
+        for (int i = unboundStart.get() - 1; i > 0; i--) {
+            if (allPages.contains(i)) {
+                unboundStart.set(i);
+                allPages.remove(i);
+            } else {
+                break;
+            }
+        }
+
+        // Now we have an organized way to iterate all allowed pages
+        // Rebuild the new set of page ranges
+        Set<PageRange> normalizedRanges = new LinkedHashSet<>();
+        int lastStart = -1;
+        int lastEnd = -1;
+        for (int pageNumber : allPages) {
+            if (lastStart == -1) {
+                lastStart = pageNumber;
+            }
+            if (lastEnd == -1 || lastEnd == pageNumber - 1) {
+                lastEnd = pageNumber;
+            } else {
+                normalizedRanges.add(new PageRange(lastStart, lastEnd));
+                lastStart = -1;
+                lastEnd = -1;
+            }
+        }
+
+        if (lastStart > 0 && lastEnd > 0) {
+            normalizedRanges.add(new PageRange(lastStart, lastEnd));
+        }
+
+        if (unboundStart.get() > 0) {
+            normalizedRanges.add(new PageRange(unboundStart.get()));
+        }
+
+        return normalizedRanges;
+    }
+
+    /**
+     * @return the {@link PageRange} set for the given string, an empty set
+     * otherwise.
      */
     public static Set<PageRange> toPageRangeSet(String selection) throws ConversionException {
         if (isNotBlank(selection)) {
@@ -54,7 +132,7 @@ public final class ConversionUtils {
                 }
                 pageRangeSet.add(range);
             }
-            return pageRangeSet;
+            return normalizePageRangeSet(pageRangeSet);
         }
         return Collections.emptySet();
     }
